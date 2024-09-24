788d7219f4b17ef93807d1a980fb291c19854116
lihahasmile <149310886+lihahasmile@users.noreply.github.com>
2024-09-24 20:06:48 +0800

    测试

--- lab1/kern/init/init.c
+++ lab1/kern/init/init.c
@@ -32,6 +32,9 @@ int kern_init(void) {
     clock_init();  // init clock interrupt
 
     intr_enable();  // enable irq interrupt
+
+    // asm("mret");// 测试非法指令异常
+    // asm("ebreak");// 测试断点异常
     
     while (1)
         ;
