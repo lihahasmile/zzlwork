ccd54f0df8903a9ad0b497462c5e8bf2007c28df
lihahasmile <149310886+lihahasmile@users.noreply.github.com>
2024-09-24 19:45:47 +0800

    Update trap.c

--- lab1/kern/trap/trap.c
+++ lab1/kern/trap/trap.c
@@ -172,7 +172,7 @@ void exception_handler(struct trapframe *tf) {
             */
             cprintf("Exception type: Breakpoint\n");
             cprintf("Exception address: 0x%08x\n", tf->epc);
-            tf->epc += 4;
+            tf->epc += 2;
             break;
         case CAUSE_MISALIGNED_LOAD:
             break;
